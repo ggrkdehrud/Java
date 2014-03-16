c5c21c1a2d04ed648d8475d6fa9799e2b3cdb4e9
Gabriel Ferreira <gabrielhws@gmail.com>
2014-03-16 20:10:52 -0300

    First

---
+++ ServletTest/src/servlet/HelloServlet.java
@@ -0,0 +1,61 @@
+package servlet;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import oracle.jrockit.jfr.settings.JSONElement;
+import sun.org.mozilla.javascript.internal.json.JsonParser;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/helloWorld")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/json");
+		
+		
+		PrintWriter out = response.getWriter();
+		out.write(ELEMENTOJSON);
+ 		out.close();
+		
+		
+		
+	}
+
+}
